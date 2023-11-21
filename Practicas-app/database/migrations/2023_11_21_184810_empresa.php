33d96dfa3c5adb335511cd3f59853dd40ccb300a
TheReeds <george159357852@gmail.com>
2023-11-21 13:53:04 -0500

    Update 2023_11_21_184810_empresa.php

--- Practicas-app/database/migrations/2023_11_21_184810_empresa.php
+++ Practicas-app/database/migrations/2023_11_21_184810_empresa.php
@@ -11,6 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+
+        });
         //
     }
 
