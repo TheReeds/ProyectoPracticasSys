ac3c5f1d611dcc65b63af1f80c71971740c7165a
TheReeds <george159357852@gmail.com>
2023-11-21 23:53:27 -0500

    Login

--- practicas-appangular/src/app/app-routing.module.ts
+++ practicas-appangular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login/login.component';
+import { RegisterComponent } from './componentes/login/register/register.component';
+import { InterfazComponent } from './componentes/login/interfaz/interfaz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InterfazComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- practicas-appangular/src/app/app.module.ts
+++ practicas-appangular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login/login.component';
+import { RegisterComponent } from './componentes/login/register/register.component';
+import { InterfazComponent } from './componentes/login/interfaz/interfaz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    InterfazComponent
   ],
   imports: [
     BrowserModule,
