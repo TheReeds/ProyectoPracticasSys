a04406e599bfe1ef01d4c40243b86a5123d9402f
pachauri2002 <omarcondor200@gmail.com>
2023-11-22 16:42:11 -0500

    summary

--- Practicas-app/database/migrations/2023_11_21_184934_carta_presentacion.php
+++ Practicas-app/database/migrations/2023_11_21_184934_carta_presentacion.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->id();
             $table->string('contenido');
-            $table->date('fecha_generacio');
+            $table->date('fecha_generacion');
         });
 
     }
