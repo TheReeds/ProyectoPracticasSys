c481eee4d71ab008dbf1c417cbba0db533f41c6f
RusselCH <103528547+RusselCH@users.noreply.github.com>
2023-11-22 17:39:05 -0500

    Update 2023_11_21_184913_supervisor.php

--- Practicas-app/database/migrations/2023_11_21_184913_supervisor.php
+++ Practicas-app/database/migrations/2023_11_21_184913_supervisor.php
@@ -11,7 +11,33 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        // Tabla de estudiantes
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            // Otros campos relacionados con el estudiante
+            $table->timestamps();
+        });
+
+        // Tabla de supervisores
+        Schema::create('supervisores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            // Otros campos relacionados con el supervisor
+            $table->timestamps();
+        });
+
+        // Tabla de asignación de supervisores a estudiantes
+        Schema::create('asignaciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('estudiante_id');
+            $table->unsignedBigInteger('supervisor_id');
+            // Agregar restricciones de clave foránea para estudiantes y supervisores
+            $table->foreign('estudiante_id')->references('id')->on('estudiantes')->onDelete('cascade');
+            $table->foreign('supervisor_id')->references('id')->on('supervisores')->onDelete('cascade');
+            // Otros campos o restricciones relacionados con la asignación
+            $table->timestamps();
+        });
     }
 
     /**
