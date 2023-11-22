80b941c4881a04a8dd21d12afccecda08a2850a4
TheReeds <george159357852@gmail.com>
2023-11-22 11:42:32 -0500

    21

--- practicas-appangular/src/main.ts
+++ practicas-appangular/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+
 import { AppModule } from './app/app.module';
-import 'alpinejs';
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
