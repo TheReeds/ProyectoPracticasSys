1bb8efe718fbe149eaeb54c65b51f592f4737293
eberthrosales <eberthrosalescastillo@gmail.com>
2023-11-22 19:58:18 -0500

    jaaaaa

--- Practicas-app/database/migrations/2023_11_21_184908_planpracticas.php
+++ Practicas-app/database/migrations/2023_11_21_184908_planpracticas.php
@@ -11,9 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('plan_practicas', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha_inicio_plan');
+            $table->date('fecha_fin_plan');
+            $table->integer('horas_planificadas');
+            $table->integer('horas_semanales_planificadas');
+            $table->string('modalidad');
+            $table->string('turno');
+            $table->string('nombre_apellido_jefe');
+            $table->text('email_jefe');
+            $table->integer('wahtsapp_jefe');
+            $table->string('tiempo_informe');
+            $table->timestamps();
+        });
     }
-
     /**
      * Reverse the migrations.
      */
