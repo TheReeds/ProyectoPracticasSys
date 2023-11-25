bbc6bce6f7f60e70ad172ac1ca82bbc67d2c6c43
TheReeds <george159357852@gmail.com>
2023-11-25 17:46:01 -0500

    21

--- Backend/database/migrations/2023_11_25_005527_create_especialidades_table.php
+++ Backend/database/migrations/2023_11_25_005527_create_especialidades_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('especialidads', function (Blueprint $table) {
+        Schema::create('especialidad', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('especialidads');
+        Schema::dropIfExists('especialidad');
     }
 };
