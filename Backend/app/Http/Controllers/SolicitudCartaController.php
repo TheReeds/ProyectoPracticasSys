9d1dd5a126f6e9f86db3eef87a026bf609210da0
TheReeds <george159357852@gmail.com>
2023-11-28 19:21:26 -0500

    reanzo

--- Backend/app/Http/Controllers/SolicitudCartaController.php
+++ Backend/app/Http/Controllers/SolicitudCartaController.php
@@ -30,19 +30,19 @@ class SolicitudCartaController extends Controller
 
     // Crear una nueva solicitud de carta
     public function store(Request $request)
-    {
-        $request->validate([
-            'estudiante_id' => 'required|exists:alumnos,id',
-            'estado' => 'required',
-            'comentarios' => 'string',
-            'cartapdfs_id' => 'required|exists:cartapdfs,id',
-            'empresa_id' => 'required|exists:empresas,id',
-        ]);
+{
+    $request->validate([
+        'estudiante_id' => 'required|exists:alumnos,id',
+        'estado' => 'required',
+        'comentarios' => 'nullable',
+        'cartapdfs_id' => 'required|exists:cartapdfs,id',
+        'empresa_id' => 'nullable|exists:empresas,id',
+    ]);
 
-        $solicitudCarta = SolicitudCarta::create($request->all());
+    $solicitud = SolicitudCarta::create($request->all());
 
-        return response()->json($solicitudCarta, 201);
-    }
+    return response()->json(['message' => 'Solicitud creada correctamente', 'data' => $solicitud], 201);
+}
 
     // Actualizar una solicitud de carta por ID
     public function update(Request $request, $id)
