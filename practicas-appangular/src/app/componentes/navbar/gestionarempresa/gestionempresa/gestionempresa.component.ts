6b28e241fd8d8c916633662b7e15fcbbe546a9d3
pachauri2002 <omarcondor200@gmail.com>
2023-11-30 12:25:57 -0500

    suummit

--- practicas-appangular/src/app/componentes/navbar/gestionarempresa/gestionempresa/gestionempresa.component.ts
+++ practicas-appangular/src/app/componentes/navbar/gestionarempresa/gestionempresa/gestionempresa.component.ts
@@ -1,28 +1,65 @@
 import { CommonModule } from '@angular/common';
-import {Component} from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { NavbarComponent } from '../../navbar.component';
-import { Route, Routes } from '@angular/router';
-import { ServicioempresaService } from '../servicioempresa.service';
+import { Route, Router, Routes } from '@angular/router';
+import { DialogService, ServicioempresaService } from '../servicioempresa.service';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 /**
  * @title Basic use of `<table mat-table>`
  */
 @Component({
-  selector: 'app-gestionempresa',
+  selector: 'app-nueva-empresa-form',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
-  templateUrl: './gestionempresa.component.html',
-  styleUrl: './gestionempresa.component.css'
-})
-export class GestionempresaComponent{
+  imports: [CommonModule, NavbarComponent, FormsModule],
+  templateUrl: './nueva-empresa-form.component.html',
+  styleUrls:['./nueva-empresa-form.component.css'],
+  providers: [DialogService],
 
+})
+
+export class NuevaEmpresaFormComponent{
+
+}
+
+export class GestionempresaComponent implements OnInit{
+
+  mostrarForm = false;
+  nuevaEmpresa: any ={};
   responseData: any;
 
-  constructor(private apiService: ServicioempresaService) { }
+  constructor(
+    private apiService: ServicioempresaService,
+    private router:Router,
+    private dialogService: DialogService
+  ) {}
 
   ngOnInit(): void {
     this.getData();
+  }
+
+  //abrirFormulario(): void{
+    //this.mostrarForm = true;
+  //}-->
+
+  abrirFormulario(): void {
+    this.dialogService.abrirFormulario();
+  }
+
+  agregarNuevaEmpresa(): void {
+    this.apiService.agregarEmpresa(this.nuevaEmpresa).subscribe(
+      (response) => {
+        console.log('Empresa agregada con éxito:', response);
+
+        this.nuevaEmpresa = {};
+        this.mostrarForm = false;
+      },
+      (error)=> {
+        console.error('Error al agregar empresa:', error);
+      }
+    );
   }
 
   getData(): void {
--- practicas-appangular/src/app/componentes/navbar/gestionarempresa/servicioempresa.service.ts
+++ practicas-appangular/src/app/componentes/navbar/gestionarempresa/servicioempresa.service.ts
@@ -2,9 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { MatDialog } from '@angular/material/dialog';
+import { NuevaEmpresaFormComponent } from './gestionempresa.component.ts';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
+export class DialogService {
+  constructor(private dialog: MatDialog) {}
+
+  abrirFormulario(): void {
+    this.dialog.open(NuevaEmpresaFormComponent, {
+      width: '80%', // Puedes ajustar el tamaño según tus necesidades
+    });
+  }
+}
+
+
 export class ServicioempresaService {
 
   private apiUrl = 'http://127.0.0.1:8000/api/empresas';
@@ -21,6 +37,9 @@ export class ServicioempresaService {
     return this.http.post<any>(this.apiUrl, data);
   }
 
-  // Agrega otros métodos según tus necesidades (PUT, DELETE, etc.)
+  agregarEmpresa(empresa: any): Observable<any> {
+    return this.http.post(this.apiUrl, empresa);
+  }
 
+  // Agrega otros métodos según tus necesidades (PUT, DELETE, etc.)
 }
